199e81b600e0cbe7e1cd62037cd11bce65540ba6
Jakub Murcek <murcja812@gmail.com>
2023-08-31 00:43:26 +0200

    movable nodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import { ipcRenderer } from "electron";
 import "./App.scss";
-import ReactFlow from "reactflow";
+import ReactFlow, {
+  Handle,
+  Position,
+  addEdge,
+  useEdgesState,
+  useNodesState,
+} from "reactflow";
 import "reactflow/dist/style.css";
+import { useCallback, useMemo } from "react";
 
 const initialNodes = [
   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  {
+    id: "2",
+    position: { x: 0, y: 100 },
+    data: { label: "2" },
+  },
 ];
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 function App() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
   return (
     <div className="App">
       <button
@@ -20,7 +38,14 @@ function App() {
         Start
       </button>
       <div style={{ width: "100vw", height: "100vh" }}>
-        <ReactFlow nodes={initialNodes} edges={initialEdges} />
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          fitView
+        />
       </div>
     </div>
   );
